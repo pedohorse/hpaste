d50d1b6f9a61e5b05aff2d11d06624df07bc7aa5
pedohorse <xapkohheh@gmail.com>
2017-12-29 00:10:32 +0300

    git collection implementation in progress, reshaping base classes

--- python2.7libs/hpaste/collectionbase.py
+++ python2.7libs/hpaste/collectionbase.py
@@ -1,32 +1,40 @@
 
+class CollectionInconsistentError(Exception):
+	def __init__(self,message):
+		super(CollectionInconsistentError,self).__init__(message)
+
 class CollectionItem(object):
 	def __init__(self,collection,name,description,id,metadata=None):
-		self.__collection=collection
-		self.__name=name			#Name
-		self.__desc=description		#Description
-		self.__id=id				#ID inside collection
-		self.__meta=metadata		#Metadata
+		self._collection=collection
+		self._name=name			#Name
+		self._desc=description		#Description
+		self._id=id				#ID inside collection
+		self._meta=metadata		#Metadata
+		self.__valid=True
 		#wid is not stored cuz it may be generated dynamically
 
 	def name(self):
-		return self.__name
+		return self._name
 
 	def description(self):
-		return self.__desc
+		return self._desc
 
 	def id(self):
-		return self.__id
+		return self._id
 
 	def metadata(self):
-		return self.__meta
+		return self._meta
 
 	def generatePublicWid(self):
-		return self.__collection.genWid(self.__id)
+		return self._collection.genWid(self)
 
 	def content(self):
-		return self.__collection.getContent(self.__id)
+		return self._collection.getContent(self)
 
 	def setName(self,newname):
+		raise NotImplementedError('Not Yet Implemented')
+
+	def setContent(self,newcontent):
 		raise NotImplementedError('Not Yet Implemented')
 
 	def setDescription(self,newname):
@@ -56,11 +64,24 @@ class CollectionBase(object):
 		#rendering all existing items invalid
 		raise NotImplementedError('Abstract method')
 
-	def genWid(self,id):
+	def genWid(self,item):
 		#this should generate hpaste compatible wid, that does not require collections to work
 		#collections can be separated from public wids, so creating a public wid is extra functionality
 		raise NotImplementedError('Abstract method')
 
-	def getContent(self,id):
+	def getContent(self,item):
 		#this should bring the raw content of the collection item.
+		raise NotImplementedError('Abstract method')
+
+	def changeItem(self,item,newName=None,newDescription=None,newContent=None):
+		#change an item
+		raise NotImplementedError('Abstract method')
+
+	def addItem(self, desiredName, description, content, metadata=None):
+		#add a new item
+		#should return CollectionItem
+		raise NotImplementedError('Abstract method')
+
+	def removeItem(self,item):
+		#remove existing item
 		raise NotImplementedError('Abstract method')
--- python2.7libs/hpaste/githubcollection.py
+++ python2.7libs/hpaste/githubcollection.py
@@ -3,7 +3,7 @@ from pprint import pprint
 import base64
 import json
 
-from collectionbase import CollectionBase,CollectionItem,defaultLogger
+from collectionbase import CollectionBase,CollectionItem,CollectionInconsistentError,defaultLogger
 log=defaultLogger
 
 class GithubCollection(CollectionBase):
@@ -15,6 +15,7 @@ class GithubCollection(CollectionBase):
 		self.__token=str(token)
 		self.__headers= {'User-Agent': 'HPaste', 'Authorization':'Token %s'%self.__token}
 		#{'Authorization': 'Basic %s' % base64.b64encode('%s:%s' % ('pedohorse', 'TentacleRapedH0lk'))}
+		#TODO: add token validity check and raise ex if not valid
 
 	def test(self):
 		req = urllib2.Request('https://api.github.com/authorizations', headers=self.__headers)
@@ -34,30 +35,26 @@ class GithubCollection(CollectionBase):
 			#TODO: resolve error
 			log("unexpected server return level %d"%rep.getcode(),3)
 			return None
-		log(rep.info())
+		log(str(rep.info()))
 		data=json.loads(rep.read())
-		gists=[x for x in data if x['description']=='HPaste snippet collection']
+		gists=[x for x in data if '00_HPASTE_SNIPPET' in x['files']]
 		if(len(gists)==0):
-			# need to create new
-			postdata={'public':False, 'description':'HPaste snippet collection'}
-			postdata['files']={'OO_SNIPPETS':{'content':'snippets marker'}}
-			req=urllib2.Request(r'https://api.github.com/gists', postdata, headers=self.__headers)
-			rep = urllib2.urlopen(req)
-			if (rep.getcode() != 200):
-				log("unexpected server return level %d" % rep.getcode(), 3)
-				return None
 			return ()
 
 		res=[]
 		for gist in gists:
 			files=gist['files']
+			try:
+				del files['00_HPASTE_SNIPPET']
+			except KeyError:
+				raise CollectionInconsistentError('impossible error! marker lost')
+			if(len(files)!=1):raise CollectionInconsistentError('each gist must have one marker and one file')
+			desc=gist['description']
+
 			for filename in files:
 				filedata=files[filename]
-				desc=''
-				if('description' in filedata):
-					desc=filedata['description']
-
-				newitem=CollectionItem(self,filename,desc,'%s@%s'%(gist['id'],filename))
+				rawurl=filedata['raw_url']
+				newitem=CollectionItem(self,filename,desc,'%s@%s'%(gist['id'],filename),{'raw_url':rawurl})
 				res.append(newitem)
 
 		return tuple(res)
@@ -69,18 +66,107 @@ class GithubCollection(CollectionBase):
 		#rendering all existing items invalid
 		raise NotImplementedError('Abstract method')
 
-	def getWid(self,id):
+	def genWid(self,item):
 		#this should generate hpaste compatible wid, that does not require collections to work
-		assert isinstance(id,str) or isinstance(id,unicode), 'id must be str or unicode'
-		id=str(id) #convert from unicode, actually id should not ever contain symbols above ord 127, so no need in encodings here
+		assert isinstance(item,CollectionItem), 'item must be a collection item'
 
-		raise NotImplementedError('Abstract method')
+		raise NotImplementedError('Not yet implemented')
+
+	def getContent(self,item):
+		#this should bring the raw content of the collection item.
+		assert isinstance(item, CollectionItem), 'item must be a collection item'
+
+		req=urllib2.Request(item.metadata()['raw_url'],headers=self.__headers)
+		rep=urllib2.urlopen(req)
+		if(rep.getcode()!=200):
+			#TODO: get some common func to process unauth errors
+			pass
+		data=rep.read()
+
+		return data
+
+	def changeItem(self, item, newName=None, newDescription=None, newContent=None):
+		assert isinstance(item, CollectionItem), 'item must be a collection item'
+		id,name=item.id().split('@',1)
+		data={}
+		proceed=False
+		if(newName is not None):
+			data['filename']=newName
+			proceed=True
+		if(newContent is not None):
+			data['content']=newContent
+			proceed = True
+		if(data!={}):
+			data={'files':{item.name():data}}
+
+		if(newDescription is not None):
+			data['description']=newDescription
+			proceed=True
+
+		if (not proceed): return
+
+		req=urllib2.Request('https://api.github.com/gists/%s'%id,json.dumps(data), headers = self.__headers)
+		req.get_method=lambda : 'PATCH'
+		rep = urllib2.urlopen(req)
+		if (rep.getcode() != 200):
+			# TODO: resolve error
+			log("unexpected server return level %d" % rep.getcode(), 3)
+			return
+
+		gist=json.loads(rep.read())
+		newfilenames=gist['files'].keys()
+		newfilenames.remove('00_HPASTE_SNIPPET')
+		if(len(newfilenames)!=1):
+			raise CollectionInconsistentError('something went wrong during item changing')
+		newfilename=newfilenames[0]
+		item._description=gist['description']
+		item._name=newfilename
+		item._meta['raw_url']=gist['files'][newfilename]['raw_url']
+		item._id='%s@%s'%(gist['id'],newfilename)
+
+
+
+	def addItem(self,desiredName,description,content,metadata=None):
+		assert isinstance(desiredName,str) or isinstance(desiredName,unicode), 'name should be a string'
+		assert isinstance(content,str) or isinstance(content,unicode), 'conetnt shoud be a string'
+
+		postdata = {'public': False, 'description': description}
+		postdata['files'] = {'00_HPASTE_SNIPPET': {'content': 'snippets marker'}}
+		postdata['files'][desiredName] = {'content':content}
+		req = urllib2.Request(r'https://api.github.com/gists', json.dumps(postdata), headers=self.__headers)
+		rep = urllib2.urlopen(req)
+		if (rep.getcode() != 201):
+			log("unexpected server return level %d" % rep.getcode(), 3)
+			return None
+
+		gist=json.loads(rep.read())
+		newfilenames = gist['files'].keys()
+		newfilenames.remove('00_HPASTE_SNIPPET')
+		if (len(newfilenames) != 1):
+			raise CollectionInconsistentError('something went wrong during item creating')
+		newfilename = newfilenames[0]
+		if(metadata is None):metadata={}
+		metadata['raw_url'] = gist['files'][newfilename]['raw_url']
+		newitem=CollectionItem(self,newfilename,gist['description'],'%s@%s' % (gist['id'], newfilename),metadata)
+		return newitem
+
+
 
 if(__name__=='__main__'):
+	from os import path
 	#testing
-	testToken = '90fe706ea9de9e401427dcb75252f006acdfb96d'
+
+	testToken = ''
+	with open(path.join(path.dirname(path.dirname(path.dirname(__file__))), 'githubtoken.tok'),'r') as f:
+		testToken=f.read()
+		testToken=testToken.replace('\n','')
+	print(testToken)
 	col=GithubCollection(testToken)
 
 	res=col.list()
 	for obj in res:
 		log(obj.id())
+	#log(res[0].content())
+
+	#log(col.changeItem(res[0]))
+
