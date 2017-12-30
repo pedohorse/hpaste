b4c66905d37b38e3e48a12875ed739ccfc590072
pedohorse <xapkohheh@gmail.com>
2017-12-30 18:46:09 +0100

    logger is more error prune

--- python2.7libs/hpaste/collections/logger.py
+++ python2.7libs/hpaste/collections/logger.py
@@ -1,7 +1,9 @@
 
 
 def simpleLogger(s,level=1):
-	levels=['VERBOSE','INFO','WARNING','ERROR']
+	levels=['VERBOSE','INFO','WARNING','ERROR','SUPER ERROR','OMEGA ERROR']
+	if(level<0):level=0
+	elif(level>=len(levels)):level=len(levels)-1
 	print('%s: %s'%(levels[level],s if isinstance(s,str) or isinstance(s,unicode) else repr(s)))
 
 def passLogger(s,level=1):
