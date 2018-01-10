b432ad31b12754ded8c1afa51a3117ac6fa0d796
pedohorse <xapkohheh@gmail.com>
2018-01-10 08:40:42 -0500

    AccountsManager added, WIP

--- python2.7libs/hpaste/accountsmanager.py
+++ python2.7libs/hpaste/accountsmanager.py
@@ -1,13 +1,21 @@
 if(__name__=='__main__'):
 	import os
-	os.environ['PATH']+=r';C:\Program Files\Side Effects Software\Houdini 16.0.600\bin'
+	import sys
+	os.environ['PATH']=r'C:\Program Files\Side Effects Software\Houdini 16.0.600\bin'+r';C:\Program Files\Side Effects Software\Houdini 16.0.600\houdini\dso;'+os.environ['PATH']
+	os.environ['HOUDINI_USER_PREF_DIR']=r'C:\Users\User\Documents\houdini16.0'
+
+
+	#print sys.path
+	from hcollections.QDoubleInputDialog import QDoubleInputDialog
+
+
 
 from PySide2.QtWidgets import *
 from PySide2.QtGui import *
 from PySide2.QtCore import *
 
 import accountsmanager_ui
-
+from githubauthorizator import GithubAuthorizator
 
 class AccountsManager(QMainWindow):
 	def __init__(self,parent):
@@ -16,6 +24,23 @@ class AccountsManager(QMainWindow):
 		self.ui=accountsmanager_ui.Ui_MainWindow()
 		self.ui.setupUi(self)
 
+		data = GithubAuthorizator.listAuthorizations()
+		self.__authModel=QStringListModel([x['user'] for x in data],self)
+		self.ui.authListView.setEditTriggers(QAbstractItemView.NoEditTriggers)
+		self.ui.authListView.setModel(self.__authModel)
+
+		data = GithubAuthorizator.listPublicCollections()
+		self.__publicModel = QStringListModel([x['user'] for x in data], self)
+		self.ui.publicListView.setEditTriggers(QAbstractItemView.NoEditTriggers)
+		self.ui.publicListView.setModel(self.__publicModel)
+
+		#slots-signals
+		self.ui.addAuthPushButton.clicked.connect(self.newAuth)
+
+
+	def newAuth(self):
+		GithubAuthorizator.newAuthorization()
+		pass
 
 
 
