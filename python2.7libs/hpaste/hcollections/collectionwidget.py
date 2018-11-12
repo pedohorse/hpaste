eaba952ebb6c9557bdb123369968a885c238fad1
pedohorse <xapkohheh@gmail.com>
2018-11-11 21:51:51 -0500

    changing snippet format, adding icon support

--- python2.7libs/hpaste/hcollections/collectionwidget.py
+++ python2.7libs/hpaste/hcollections/collectionwidget.py
@@ -146,18 +146,24 @@ class SnippetCollectionModel(QAbstractTableModel):
 		return self.createIndex(row,col,self.__itemList[row])
 
 	def data(self,index, role = Qt.DisplayRole):
-		if(role==Qt.DisplayRole):
-			if(index.column() == 0):
+		if role == Qt.DisplayRole:
+			if index.column() == 0:
 				return self.__itemList[index.row()].name()
-			elif(index.column() == 1):
+			elif index.column() == 1:
 				return self.__itemList[index.row()].description()
-			elif(index.column() == 2):
+			elif index.column() == 2:
 				return self.__itemList[index.row()].id()
 			else:
 				metadata=self.__itemList[index.row()].metadata()
 				key=self.__metadataExposedKeys[index.column()-3]
 				if(key in metadata): return metadata[key]
 				else: return ''
+		elif role == Qt.DecorationRole and 'iconpixmap' in self.__itemList[index.row()].metadata():
+			print "Foo"
+			pixmap = self.__itemList[index.row()].metadata()['iconpixmap']
+			print pixmap
+			return pixmap
+
 		return None
 
 
--- python2.7libs/hpaste/hcollections/githubcollection.py
+++ python2.7libs/hpaste/hcollections/githubcollection.py
@@ -2,7 +2,18 @@ import urllib2
 import json
 import copy
 
+import re
+
 from collectionbase import CollectionBase,CollectionItem,CollectionInconsistentError,CollectionSyncError,CollectionItemInvalidError,CollectionItemReadonlyError,CollectionReadonlyError
+
+qtAvailable = False
+try:
+	from PySide2.QtGui import QPixmap
+	qtAvailable = True
+except ImportError:
+	from PySide.QtGui import QPixmap
+	qtAvailable = True
+
 
 from logger import defaultLogger as log
 
@@ -25,6 +36,24 @@ class InvalidToken(CollectionSyncError):
 	def __init__(self,message):
 		super(InvalidToken,self).__init__(message)
 
+
+class Cacher(object):
+	def __init__(self):
+		self.__cache = {}
+
+	def __contains__(self, item):
+		return item in self.__cache
+
+	def __getitem__(self, item):
+		return self.__cache[item]
+
+	def __setitem__(self, key, value):
+		self.__cache[key] = value
+
+	def clear(self):
+		self.__cache.clear()
+
+globalIconCacher = Cacher()
 
 class GithubCollection(CollectionBase):
 	def __init__(self, token_or_username, public=False, token_for_public_access=None):
@@ -86,18 +115,57 @@ class GithubCollection(CollectionBase):
 				del files['00_HPASTE_SNIPPET']
 			except KeyError:
 				raise CollectionInconsistentError('impossible error! marker lost')
-			if(len(files)!=1):raise CollectionInconsistentError('each gist must have one marker and one file')
+			#if(len(files)!=1):raise CollectionInconsistentError('each gist must have one marker and one file')
 			desc=gist['description']
 			nettype=''
 			if(':' in desc):
 				nettype,desc=desc.split(':',1)
 
-			for filename in files:
-				filedata=files[filename]
-				rawurl=filedata['raw_url']
+			if(len(files)==1): # zero version implementation
+				for filename in files:
+					filedata=files[filename]
+					rawurl=filedata['raw_url']
+					retaccess = CollectionItem.AccessType.public if gist['public'] else CollectionItem.AccessType.private
+					newitem=CollectionItem(self, filename, desc, '%s@%s'%(gist['id'], filename), retaccess, self.__readonly, metadata={'raw_url':rawurl,'nettype':nettype})
+					res.append(newitem)
+			else:
+				itemfiles = [x for x in files if x.startswith("item:")]
+				if len(itemfiles) != 1: raise CollectionInconsistentError('could not find unique item data in gist')
+				filedata = files[itemfiles[0]]
+				itemname = itemfiles[0].split(':')[1]
+				rawurl = filedata['raw_url']
 				retaccess = CollectionItem.AccessType.public if gist['public'] else CollectionItem.AccessType.private
-				newitem=CollectionItem(self, filename, desc, '%s@%s'%(gist['id'], filename), retaccess, self.__readonly, metadata={'raw_url':rawurl,'nettype':nettype})
+				newitem = CollectionItem(self, itemname, desc, '%s@%s'%(gist['id'], itemfiles[0]), retaccess, self.__readonly, metadata={'raw_url':rawurl,'nettype':nettype})
+
+				for typefilename in files:
+					if ':' not in typefilename: continue
+					filetype, filename = typefilename.split(':', 1)
+					if filetype == 'item': continue  # Skip item file, it was already processed
+					if filetype == 'icon':
+						if not qtAvailable: continue  # If we were loaded from non-interactive session - just skip icons
+
+						if ':' not in filename: continue
+						icontype, iconname =  filename.split(':', 1)
+						if icontype not in ['XPM']: continue  # supported icon format list
+
+						filedata = files[typefilename]
+						url = filedata['raw_url']
+						if url in globalIconCacher:
+							data = globalIconCacher[url]
+						else:
+							req = urllib2.Request(url, headers=self.__headers)
+							code, rep = urlopen_nt(req)
+							if (code != 200):
+								if code == 403: raise InvalidToken('github auth failed')
+								raise CollectionSyncError("unexpected server return level %d" % code)
+							data = rep.read()
+
+						if icontype == 'XPM':
+							xpmlines = map(lambda x: x.replace('"', ''), re.findall('"[^"]*"', data))
+							newitem.metadata()['iconpixmap'] = QPixmap(xpmlines)
+
 				res.append(newitem)
+
 
 		return tuple(res)
 
@@ -136,11 +204,11 @@ class GithubCollection(CollectionBase):
 		#newContent=str(newContent)
 		#just in case we have random unicode coming in
 		#TODO: check that item belongs to this collection. just in case
-		if (item.readonly()):raise CollectionReadonlyError()
-		if (newAccess is not None and newAccess!=item.access()):
+		if item.readonly():raise CollectionReadonlyError()
+		if newAccess is not None and newAccess!=item.access():
 			#raise NotImplementedError('not yet implemented')
 
-			newitem=self.addItem(item.name() if newName is None else newName, item.description() if newDescription is None else newDescription, item.content() if newContent is None else newContent,newAccess,item.metadata())
+			newitem=self.addItem(item.name() if newName is None else newName, item.description() if newDescription is None else newDescription, item.content() if newContent is None else newContent,newAccess, item.metadata())
 			self.removeItem(copy.copy(item))  # remove the copy cuz item gets invalidated and we dont want that for original item
 			item._name = newitem._name
 			item._desc = newitem._desc
@@ -149,29 +217,35 @@ class GithubCollection(CollectionBase):
 			item._id = newitem._id
 			item._access=newitem._access
 			item._readonly=newitem._readonly
+			# TODO: all auxiliary files MUST BE COPIED !!
 
 			# if access is changed - we have to destroy this gist and create a new one with proper 'public' key
 			# Butt Beware - we need to modify item's contents and return it WITHOUT reassigning the item itself
 			# That's what we are doing here currently
 
-		if('nettype' not in item.metadata()):
+			# TODO: do we need to continue at this point? all the fields seem to be changed already
+
+		if 'nettype' not in item.metadata():
 			item._invalidate()
 			raise CollectionItemInvalidError('required metadata was not found in the item')
 
-		id,name=item.id().split('@',1)
+		id, filename = item.id().split('@',1)
 		data={}
 		proceed=False
 		if(newName is not None):
-			data['filename']=newName
+			if filename.startswith('item:'):  # new format. can cause trouble if someone actually called item starting with 'item:'
+				data['filename'] = 'item:'+newName
+			else:  # old format
+				data['filename'] = newName
 			proceed=True
 		if(newContent is not None):
-			data['content']=newContent
+			data['content'] = newContent
 			proceed = True
 		if(data!={}):
-			data={'files':{item.name():data}}
+			data={'files':{filename:data}}
 
 		if(newDescription is not None):
-			data['description']=':'.join((item.metadata()['nettype'],newDescription))
+			data['description']=':'.join((item.metadata()['nettype'], newDescription))
 			proceed=True
 
 		if (not proceed): return
@@ -186,20 +260,25 @@ class GithubCollection(CollectionBase):
 		gist=json.loads(rep.read())
 		newfilenames=gist['files'].keys()
 		newfilenames.remove('00_HPASTE_SNIPPET')
-		if(len(newfilenames)!=1):
-			raise CollectionInconsistentError('something went wrong during item changing')
-		newfilename=newfilenames[0]
+		if(len(newfilenames) == 1):
+			newfilename=newfilenames[0]
+			newname = newfilename
+		else:
+			itemfileanmes = [x for x in newfilenames if x.startswith("item:")]
+			if len(itemfileanmes) != 1: raise CollectionInconsistentError('something went wrong during item creation: could not find unique item data in gist')
+			newfilename = itemfileanmes[0]
+			newname = newfilename.split(':', 1)[1]
 		desc = gist['description']
 		nettype = ''
 		if (':' in desc):
 			nettype, desc = desc.split(':', 1)
-		item._desc=desc
-		item._name=newfilename
-		item._meta['raw_url']=gist['files'][newfilename]['raw_url']
-		item._meta['nettype']=nettype
-		item._id='%s@%s'%(gist['id'],newfilename)
-		item._access= CollectionItem.AccessType.public if gist['public'] else CollectionItem.AccessType.private
-		item._readonly=False
+		item._desc = desc
+		item._name = newname
+		item._meta['raw_url'] = gist['files'][newfilename]['raw_url']
+		item._meta['nettype'] = nettype
+		item._id = '%s@%s'%(gist['id'],newfilename)
+		item._access = CollectionItem.AccessType.public if gist['public'] else CollectionItem.AccessType.private
+		item._readonly = False
 
 
 
@@ -216,7 +295,9 @@ class GithubCollection(CollectionBase):
 		description=":".join((metadata['nettype'],description))
 		postdata = {'public': access==CollectionItem.AccessType.public, 'description': description}
 		postdata['files'] = {'00_HPASTE_SNIPPET': {'content': 'snippets marker'}}
-		postdata['files'][desiredName] = {'content':content}
+		postdata['files']['item:'+desiredName] = {'content':content}
+		postdata['files']['ver'] = {'content':'1.1'}
+
 		req = urllib2.Request(r'https://api.github.com/gists', json.dumps(postdata), headers=self.__headers)
 		code, rep = urlopen_nt(req)
 		if (code != 201):
@@ -226,18 +307,24 @@ class GithubCollection(CollectionBase):
 		gist=json.loads(rep.read())
 		newfilenames = gist['files'].keys()
 		newfilenames.remove('00_HPASTE_SNIPPET')
-		if (len(newfilenames) != 1):
-			raise CollectionInconsistentError('something went wrong during item creating')
-		newfilename = newfilenames[0]
-		if(metadata is None):metadata={}
+		if (len(newfilenames) == 1):
+			newfilename = newfilenames[0]
+			newname = newfilename
+		else:
+			itemfileanmes = [x for x in newfilenames if x.startswith("item:")]
+			if len(itemfileanmes) != 1: raise CollectionInconsistentError('something went wrong during item creation: could not find unique item data in gist')
+			newfilename = itemfileanmes[0]
+			newname = newfilename.split(':',1)[1]
+
+		if metadata is None: metadata={}
 		metadata['raw_url'] = gist['files'][newfilename]['raw_url']
-		desc=gist['description']
+		desc = gist['description']
 		nettype = ''
 		if (':' in desc):
 			nettype, desc = desc.split(':', 1)
-		metadata['nettype']=nettype
-		retaccess=CollectionItem.AccessType.public if gist['public'] else CollectionItem.AccessType.private
-		newitem=CollectionItem(self,newfilename,desc,'%s@%s' % (gist['id'], newfilename),retaccess,False,metadata)
+		metadata['nettype'] = nettype
+		retaccess = CollectionItem.AccessType.public if gist['public'] else CollectionItem.AccessType.private
+		newitem = CollectionItem(self, newname, desc, '%s@%s' % (gist['id'], newfilename), retaccess, False, metadata)
 		return newitem
 
 	def removeItem(self,item):
