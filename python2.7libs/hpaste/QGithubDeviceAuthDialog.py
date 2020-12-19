538d837f088711c03ee122deea3e0331bad4e22b
pedohorse <xapkohheh@gmail.com>
2020-12-19 15:56:05 +0100

    implemented some error handling

--- python2.7libs/hpaste/QGithubDeviceAuthDialog.py
+++ python2.7libs/hpaste/QGithubDeviceAuthDialog.py
@@ -3,7 +3,7 @@ import json
 import urllib2
 from nethelper import urlopen_nt
 try:
-    from PySide2.QtWidgets import QDialog, QVBoxLayout, QLabel, QSizePolicy, QPushButton
+    from PySide2.QtWidgets import QDialog, QVBoxLayout, QLabel, QSizePolicy, QPushButton, QMessageBox
     from PySide2.QtWebEngineWidgets import QWebEngineView, QWebEngineProfile, QWebEnginePage
     from PySide2.QtCore import Slot, Qt, QUrl
 except ImportError:
@@ -21,9 +21,11 @@ class QGithubDeviceAuthDialog(QDialog):
         self.__webview.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
         self.__webview.urlChanged.connect(self.on_url_changed)
 
-        self.__infolaabel = QLabel('<br>You need to allow hpaste to modify your gists on github. for that you need to log in to your account and authorise hpaste.\n'
+        self.__infolaabel = QLabel('<p style="font-size:14px">'
+                                   '<br>You need to allow hpaste to modify your gists on github. for that you need to log in to your account and authorize hpaste.\n'
                                    '<br>You can do it in any browser, just go to <a href="https://github.com/login/device">https://github.com/login/device</a> and enter the code below.\n'
-                                   '<br>close this window when you are done', parent=self)
+                                   '<br>close this window when you are done'
+                                   '</p>', parent=self)
         self.__infolaabel.setTextFormat(Qt.RichText)
         self.__infolaabel.setTextInteractionFlags(Qt.TextBrowserInteraction)
         self.__infolaabel.setOpenExternalLinks(True)
@@ -44,9 +46,25 @@ class QGithubDeviceAuthDialog(QDialog):
         self.__layout.addWidget(self.__webview)
         self.__result = None
 
+        # self.setGeometry(512, 256, 1024, 768)
+        self.resize(1024, 860)
+
         # init auth process
         self.__client_id = client_id
         self.__headers = {'User-Agent': 'HPaste', 'Accept': 'application/json', 'Content-Type': 'application/json'}
+        self.__hint_username = hint_username
+
+        self.__webprofile = QWebEngineProfile('empty', parent=self)
+
+        self.__webpage = QWebEnginePage(self.__webprofile, parent=self.__webprofile)  # just to be sure they are deleted in proper order
+        self.__webview.setPage(self.__webpage)
+
+        self.__device_code = None
+        self.__interval = None
+        self.__await_login_redirect = False
+        self.reinit_code()
+
+    def reinit_code(self):
         reqdata = {'client_id': self.__client_id,
                    'scope': 'gist'}
         req = urllib2.Request('https://github.com/login/device/code', data=json.dumps(reqdata), headers=self.__headers)
@@ -58,19 +76,14 @@ class QGithubDeviceAuthDialog(QDialog):
         print(init_data)
         self.__device_code = init_data['device_code']
         self.__interval = init_data.get('interval', 5)
-        self.__webprofile = QWebEngineProfile('empty', parent=self)
+        url = init_data['verification_uri']
+
+        self.__await_login_redirect = True
         self.__webprofile.clearHttpCache()
         self.__webprofile.cookieStore().deleteAllCookies()
-        self.__webpage = QWebEnginePage(self.__webprofile, parent=self.__webprofile)  # just to be sure they are deleted in proper order
-        self.__webview.setPage(self.__webpage)
-        url = init_data['verification_uri']
-        self.__hint_username = hint_username
-        self.__await_login_redirect = True
         self.__webview.load(QUrl(url))
         self.__devidlabel.setText('code: %s' % (init_data['user_code'],))
 
-        #self.setGeometry(512, 256, 1024, 768)
-        self.resize(1024, 768)
 
     def get_result(self):
         return self.__result
@@ -105,25 +118,28 @@ class QGithubDeviceAuthDialog(QDialog):
                 # NO SUCC
                 errcode = rep['error']
                 if errcode == 'authorization_pending':  # note that this error will happen if user just closes down the window
-                    self.__result = None
                     break
                 elif errcode == 'slow_down':
                     self.__interval = rep.get('interval', self.__interval + 5)
                     time.sleep(self.__interval)
                     continue
                 elif errcode == 'expired_token':
-                    # TODO: alert user, restart the process
-                    raise NotImplementedError()
+                    if QMessageBox.warning(self, 'device code expired', 'it took you too long to enter the code, now it\'s expired.\nWant to retry?', buttons=QMessageBox.Yes | QMessageBox.No) == QMessageBox.Yes:
+                        event.reject()
+                        self.reinit_code()
+                        return
                 elif errcode == 'unsupported_grant_type':
                     raise RuntimeError('unsupported grant type. probably github changed API. need to update the plugin')
                 elif errcode == 'incorrect_client_credentials':
                     raise RuntimeError('incorect client id. probably pedohorse changed hpaste id for some reason. update the plugin')
                 elif errcode == 'incorrect_device_code':
-                    # TODO: alert user, restart the process
-                    raise NotImplementedError()
+                    if QMessageBox.warning(self, 'bad device code', 'server reported wrong device code\nWant to retry?', buttons=QMessageBox.Yes | QMessageBox.No) == QMessageBox.Yes:
+                        event.reject()
+                        self.reinit_code()
+                        return
                 elif errcode == 'access_denied':
                     # means user denied the request
-                    self.__result = None
+                    pass
                 else:
                     raise RuntimeError('unexpected error: %s' % (json.dumps(rep),))
             else:
@@ -153,8 +169,7 @@ class QGithubDeviceAuthDialog(QDialog):
     @Slot()
     def reload_button_clicked(self):
         #QWebEngineProfile.defaultProfile().cookieStore().deleteAllCookies()
-        self.__webview.page().profile().cookieStore().deleteAllCookies()
-        self.__webview.reload()
+        self.reinit_code()
 
 
 if __name__ == '__main__':  # testing
