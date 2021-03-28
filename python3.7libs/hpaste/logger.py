22843d61f8405701e2ba3a8435fee3fa5a8145e6
pedohorse <xapkohheh@gmail.com>
2021-03-28 17:19:45 +0200

    unicode class removed

--- python3.7libs/hpaste/logger.py
+++ python3.7libs/hpaste/logger.py
@@ -5,7 +5,7 @@ def simpleLogger(s,level=1):
 	levels=['VERBOSE','INFO','WARNING','ERROR','SUPER ERROR','OMEGA ERROR']
 	if(level<0):level=0
 	elif(level>=len(levels)):level=len(levels)-1
-	print('%s: %s'%(levels[level],s if isinstance(s,str) or isinstance(s,unicode) else repr(s)))
+	print('%s: %s' % (levels[level], s))
 
 def passLogger(s,level=1):
 	pass
