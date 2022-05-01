e51c25518abd1e1a33f3e4ab4d81dc3499efe612
pedohorse <xapkohheh@gmail.com>
2022-05-01 21:53:28 +0200

    added new scan_for_authors_dates function

--- python2.7libs/hpaste/codeanalysis.py
+++ python2.7libs/hpaste/codeanalysis.py
@@ -1,3 +1,4 @@
+import sys
 import re
 
 from .hpaste import stringToData, WrongKeyError
@@ -28,9 +29,16 @@ pyimports = re.compile(br'(?:from(?:\s|\\\s)+'
 
 nodeinit = re.compile(b'(\\w+(?:/\\w+)?)\\.init\0(?:type\s+=\s+(\w+))?')
 
+statmeta = re.compile(br'stat\s*'
+                      br'\{'
+                      br'\s*create\s+(?P<create>\d+)\n'
+                      br'\s*modify\s+(?P<modify>\d+)\n'
+                      br'\s*author\s+(?P<author>.+)@(?P<hostname>.+)\n'
+                      br'\s*access\s+(?P<access>\d+)\n'
+                      br'\s*\}', re.MULTILINE)
 
-def _lic_type_from_code(code):
-    # type: (bytes) -> str
+def _lic_type_from_code(code, data):
+    # type: (bytes, dict) -> str
     lic = 'unknown'
     if cheader.match(code):
         lic = 'Commercial'
@@ -38,11 +46,12 @@ def _lic_type_from_code(code):
         match = ncheader.match(code)
         if match:
             lic = 'Non Commercial' if match.group('lic') == 'HouNC' else 'Limited Commercial'
+    data['license'] = lic
     return lic
 
 
-def _scan_for_python_imports(code):
-    # type: (bytes) -> set[str]
+def _scan_for_python_imports(code, data):
+    # type: (bytes, dict) -> set[str]
     imports = set()
     for match in pyimports.finditer(code):
         pref = None
@@ -54,17 +63,18 @@ def _scan_for_python_imports(code):
         if pref:
             imps = ['.'.join((pref, imp)) for imp in imps]
         imports.update(imps)
+    data['imports'] = imports
     return imports
 
 
-def _scan_for_nodes(code):
-    # type: (bytes) -> list[(str, str)]
+def _scan_for_nodes(code, data):
+    # type: (bytes, dict) -> list[(str, str)]
     """
     this is a very approximate crude method
     """
     nodes = []
 
-    lic = _lic_type_from_code(code)
+    lic = _lic_type_from_code(code, {})
     if lic == 'unknown':
         raise RuntimeError('unknown license type')
     elif lic == 'Commercial':
@@ -75,7 +85,29 @@ def _scan_for_nodes(code):
         nmatch = nodeinit.match(code, pos=match.end())
         if nmatch:
             nodes.append((nmatch.group(1).decode('UTF-8'), nmatch.group(2).decode('UTF-8') if nmatch.group(2) else '<N/A>'))
+    data['nodes'] = nodes
     return nodes
+
+
+def _scan_for_authors_dates(code, data):
+    # type: (bytes, dict) -> tuple[set[tuple[str, str]], tuple[tuple[int, int], tuple[int, int]]]
+    authors = set()
+    min_c_time = sys.maxsize
+    min_m_time = sys.maxsize
+    max_c_time = 0
+    max_m_time = 0
+    for match in statmeta.finditer(code):
+        authors.add((match.group('author').decode('UTF-8'), match.group('hostname').decode('UTF-8')))
+        ctime = int(match.group('create'))
+        mtime = int(match.group('modify'))
+        min_c_time = min(min_c_time, ctime)
+        max_c_time = max(max_c_time, ctime)
+        min_m_time = min(min_m_time, mtime)
+        max_m_time = max(max_m_time, mtime)
+    data['authors'] = authors
+    data['nctimes'] = (min_c_time, max_c_time)
+    data['nmtimes'] = (min_m_time, max_m_time)
+    return authors, ((min_c_time, max_c_time), (min_m_time, max_m_time))
 
 
 def generate_report_data(s, key=None):
@@ -91,11 +123,16 @@ def generate_report_data(s, key=None):
     except WrongKeyError:
         return {'error': 'cannot deserialize data, wrong encryption key provided'}
 
+    return generate_report_data_for_code(code)
+
+
+def generate_report_data_for_code(code):
     info = {}
 
-    info['license'] = _lic_type_from_code(code)
-    info['imports'] = _scan_for_python_imports(code)
-    info['nodes'] = _scan_for_nodes(code)
+    _lic_type_from_code(code, info)
+    _scan_for_python_imports(code, info)
+    _scan_for_nodes(code, info)
+    _scan_for_authors_dates(code, info)
 
     return info
 
@@ -108,7 +145,7 @@ def _test_scan_for_python_imports():
         wowus.\
         titus
 andmorethings'''
-    test = _scan_for_python_imports(text)
+    test = _scan_for_python_imports(text, {})
     assert test == {'ass.pop.mouth', 'ass.pop.wowus.titus'}, test
 
 
@@ -120,11 +157,38 @@ HouLC\x1a1033600bbb0626e4915076ac4ae7geo1/merge11.userdata\0\0\0\0\1\0
 ___Version___\0\0\0\x03\0\x0818.5.408HouLC\x1a1033600bbc0626e491507277fc71geo1/group9.init\0type = groupcreate
 matchesdef = 0
 HouLC\x1a1033600bbd0626e4915015968140geo1/group9.def\0sopflags sopflags = '''
-    test = _scan_for_nodes(text)
-    assert test == {'geo1/group9'}, test
+    test = _scan_for_nodes(text, {})
+    assert test == [('geo1/group9', 'groupcreate')], test
 
+
+def _test_scan_author_meta():
+    text = b'''outputsNamed3
+{
+}
+inputsNamed3
+{
+}
+inputs
+{
+}
+stat
+{
+  create 1656665358
+  modify 1656665364
+  author joey@lucy
+  access 0777
+}
+color UT_Color RGB 0.8 0.8 0.8 
+delscript ""
+exprlanguage hscript
+end
+0707070000010000000006660000000000000000010000001423273142300001500000000243python1.parm\0{
+version 0.8'''
+    test = _scan_for_authors_dates(text, {})
+    assert test == ({('joey', 'lucy')}, ((1656665358, 1656665358), (1656665364, 1656665364))), test
 
 if __name__ == '__main__':
     _test_scan_for_python_imports()
     _test_scan_for_nodes()
+    _test_scan_author_meta()
     print('all tests are fine')
